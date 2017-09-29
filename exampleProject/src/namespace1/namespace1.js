7af79413279d034416c11c20d88a572bb8cfe335
mwehrstedt <maxmilian.wehrstedt@outlook.de>
2017-09-29 18:23:26 +0200

    added different function declarations

--- exampleProject/src/namespace1/namespace1.js
+++ exampleProject/src/namespace1/namespace1.js
@@ -7,12 +7,13 @@ var myModule_Namespace1 = (function () {
 
 	/**
 	 * Executes some fancy commands
-	 * @param {string} param1 Some string parameter
+	 * @param {string|object} param1 Some string parameter
 	 * @param {string[]} param2 Some string array
+	 * @param {DocFile} param3 fgfg
 	 * @returns {object} a fancy object
 	 * @memberof myModule.myModule_Namespace1
 	 */
-	function function1(param1, param2) {
+	function function1(param1, param2, param3) {
 
 	}
 
--- exampleProject/src/namespace2/namespace2.js
+++ exampleProject/src/namespace2/namespace2.js
@@ -7,6 +7,8 @@ var myModule_Namespace2 = (function () {
 
 	/**
 	 * Destroys your system
+	 * @param {string} param1 A simple string param
+	 * @param {object} param2 A object param
 	 * @returns {string} A funny joke about you
 	 * @memberof myModule.myModule_Namespace2
 	 */
@@ -14,8 +16,36 @@ var myModule_Namespace2 = (function () {
 
 	}
 
+	/**
+	 * A simple function
+	 * @memberof myModule.myModule_Namespace2
+	 */
+	function function2() {
+
+	}
+
+	/**
+	 * A function with multiple return values
+	 * @memberof myModule.myModule_Namespace2
+	 * @returns {string|boolean}
+	 */
+	function function3() {
+
+	}
+
+	/**
+	 * Function with different parameter types
+	 * @param {string|boolean} param1 A fancy parameter
+	 */
+	function function4(param1) {
+
+	}
+
 	return {
-		function1: function1
+		function1: function1,
+		function2: function2,
+		function3: function3,
+		function4: function4
 	};
 
 })();
