d6cba94c971580f4bd3a5b4e6ea64158e32e258f
mwehrstedt <maxmilian.wehrstedt@outlook.de>
2017-09-30 14:22:49 +0200

    divided test case file to multiple test suites

---
+++ src/test/namespace/test.parse_namespaces.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import * as dom from "dts-dom";
+import * as fs from "fs";
+import * as path from "path";
+import { JSDocTsdParser } from "../../core/jsdoc-tsd-parser";
+
+describe("JSDocTsdParser.parse.namespace", () => {
+	it("should create a namespace dom object", () => {
+		let namespace: INamespaceDoclet = JSON.parse(fs.readFileSync(path.resolve(__dirname, "data/namespace.json"), { encoding: "utf-8" }));
+		let parser = new JSDocTsdParser();
+		parser.parse([namespace]);
+
+		let result = parser.getResultItems();
+		expect(result).haveOwnProperty(namespace.longname);
+
+		let namespaceDeclarations = result[namespace.longname];
+		expect(namespaceDeclarations.length).to.equals(1);
+
+		let namespaceDeclaration = namespaceDeclarations[0] as dom.NamespaceDeclaration;
+		expect(namespaceDeclaration.kind).to.equals("namespace");
+		expect(namespaceDeclaration.name).to.equals(namespace.longname);
+		expect(namespaceDeclaration.members.length).to.equals(0);
+	});
+});
