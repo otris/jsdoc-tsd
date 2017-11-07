dfd5f3a9110b059e1fced71113b7e01823d9f658
Maximilian Wehrstedt <wehrstedt@otris.de>
2017-11-07 14:21:09 +0100

    test: parse members

---
+++ src/test/member/test.parseMember.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import * as dom from "dts-dom";
+import * as fs from "fs";
+import * as path from "path";
+import { JSDocTsdParser } from "../../core/jsdoc-tsd-parser";
+import { parse } from "querystring";
+
+describe("JSDocTsdParser.parse.member", () => {
+	let classData: TDoclet[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, "data/classMember.json"), { encoding: "utf-8" }));
+	expect(classData.length).to.eq(3);
+
+	it("should create a class with a number member", () => {
+		let parser = new JSDocTsdParser();
+		parser.parse(classData);
+
+		let result = parser.prepareResults();
+		expect(result).haveOwnPropertyDescriptor("myTestClass");
+
+		let myClass: dom.ClassDeclaration = result["myTestClass"] as dom.ClassDeclaration;
+		expect(myClass.members.length).to.equal(1);
+
+		let myPropertyMember: dom.PropertyDeclaration = myClass.members[0] as dom.PropertyDeclaration;
+		expect(myPropertyMember.name).to.eq("myNumberMember");
+		expect(myPropertyMember.jsDocComment).to.eq("A simple number member");
+		expect(myPropertyMember.type).to.eq(dom.type.number);
+	});
+
+});
