cf96a220e2fb0f3d568785d3fe6f22a1198159c8
Maximilian Wehrstedt <wehrstedt@otris.de>
2017-11-07 14:43:16 +0100

    test: test suite for parsing the example project

---
+++ src/test/test.example.ts
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+import * as dom from "dts-dom";
+import * as fs from "fs";
+import * as path from "path";
+import { JSDocTsdParser } from "../core/jsdoc-tsd-parser";
+
+describe("JSDocTsdParser.parse.exampleProject", () => {
+	it("Should parse the example project", () => {
+		let data: TDoclet[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../exampleProject/jsdoc-results.json"), { encoding: "utf-8" }));
+
+		let parser = new JSDocTsdParser();
+		parser.parse(data);
+		let result = parser.resolveResults();
+		console.log(result);
+	});
+})
