e026a643ef005634e711a5bb3dd5abccea57fc1d
Christine Heller <heller@otris.de>
2017-11-24 08:51:48 +0100

    add test for description and example tags in comment

---
+++ src/test/description/data/description.js
@@ -0,0 +1,18 @@
+/**
+ * @class MyClass
+ */
+/**
+* @function myFunction
+* @param {string} myParamter My string parameter
+* @returns {boolean} My return value
+* @memberof MyClass
+* @summary my summary should be ignored
+* @description My long description
+* first line
+* second line
+* third line
+* @example
+* my example first line
+* my example second line
+* my example third line
+**/
