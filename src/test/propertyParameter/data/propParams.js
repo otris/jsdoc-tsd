0e57244502b86bd61d992fd062ead662aa0ee2d5
Christine Heller <heller@otris.de>
2018-06-15 08:17:48 +0200

    Add test for array-parameters with properties

--- src/test/propertyParameter/data/propParams.js
+++ src/test/propertyParameter/data/propParams.js
@@ -10,3 +10,12 @@
  * @param {string} propertyParameter.myProperty1 Description of myProperty1
  * @param {number} propertyParameter.myProperty2 Description of myProperty2
  */
+
+ /**
+ * Assign the project to a list of employees
+ * @memberof myTestInterface
+ * @function myEmployeeFunction
+ * @param {Object[]} employees - The employees who are responsible for the project
+ * @param {string} employees[].name - The name of an employee
+ * @param {string} employees[].department - The employee's department
+ */
--- src/test/propertyParameter/test.propParams.ts
+++ src/test/propertyParameter/test.propParams.ts
@@ -7,7 +7,7 @@ import { parse } from "querystring";
 
 describe("JSDocTsdParser.parse.parameterWithProperties", () => {
 	let interfaceData: TDoclet[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, "data/propParams.json"), { encoding: "utf-8" }));
-	expect(interfaceData.length).to.eq(3);
+	expect(interfaceData.length).to.eq(4);
 
 	it("should create a function with a parameter with properties", () => {
 		let parser = new JSDocTsdParser();
@@ -18,7 +18,7 @@ describe("JSDocTsdParser.parse.parameterWithProperties", () => {
 		expect(result).haveOwnPropertyDescriptor("mySimpleFunction_propertyParameter");
 		
 		let parsedInterface: dom.InterfaceDeclaration = result["myTestInterface"] as dom.InterfaceDeclaration;
-		expect(parsedInterface.members.length).to.eq(1);
+		expect(parsedInterface.members.length).to.eq(2);
 
 		let paramInterface: dom.InterfaceDeclaration = result["mySimpleFunction_propertyParameter"] as dom.InterfaceDeclaration;
 
@@ -29,6 +29,29 @@ describe("JSDocTsdParser.parse.parameterWithProperties", () => {
 		expect(parameters.length).to.eq(1);
 
 		let interfaceType = dom.create.typeParameter("mySimpleFunction_propertyParameter", paramInterface);
+		expect(parameters[0].type).to.eql(interfaceType);
+	});
+
+	it("should create a function with an array of parameters with properties", () => {
+		let parser = new JSDocTsdParser();
+		parser.parse(interfaceData);
+
+		let result = parser.prepareResults();
+		expect(result).haveOwnPropertyDescriptor("myTestInterface");
+		expect(result).haveOwnPropertyDescriptor("myEmployeeFunction_employees");
+		
+		let parsedInterface: dom.InterfaceDeclaration = result["myTestInterface"] as dom.InterfaceDeclaration;
+		expect(parsedInterface.members.length).to.eq(2);
+
+		let paramInterface: dom.InterfaceDeclaration = result["myEmployeeFunction_employees"] as dom.InterfaceDeclaration;
+
+		let methodDeclaration: dom.MethodDeclaration = parsedInterface.members[1] as dom.MethodDeclaration;
+		expect(methodDeclaration.jsDocComment).to.eq("Assign the project to a list of employees\n@param employees - The employees who are responsible for the project\n@param employees[].name - The name of an employee\n@param employees[].department - The employee's department");
+
+		let parameters: dom.Parameter[] = methodDeclaration.parameters;
+		expect(parameters.length).to.eq(1);
+
+		let interfaceType = dom.create.array(paramInterface);
 		expect(parameters[0].type).to.eql(interfaceType);
 	});
 });
