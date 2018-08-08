790e8568ec8dd3c64201ecdeaf521bd1d0c1afa8
Christine Heller <heller@otris.de>
2018-08-08 11:39:54 +0200

    fixes for callbacks and inner members in modules

---
+++ exampleProject/src/core/myModule3.js
@@ -0,0 +1,21 @@
+/**
+ * @module myModule3
+ */
+module.exports = {
+    /**
+     * exported function
+     * @param {string} param1
+     * @return {string}
+     */
+    myFunction1: function(param1, color2) {
+        // ...
+    },
+    /**
+     * inner function
+     * @param {string} param2
+     * @inner
+     */
+    myFunction2: function(param2) {
+        // ...
+    },
+};
