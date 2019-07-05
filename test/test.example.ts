2cac432652bcb30e6baf417e18da9da50f440bb2
Maximilian Wehrstedt <wehrstedt@otris.de>
2019-07-05 09:28:29 +0200

    Fixed linting errors

--- test/test.example.ts
+++ test/test.example.ts
@@ -1,14 +1,12 @@
-import { expect } from "chai";
 import { execSync } from "child_process";
-import * as dom from "dts-dom";
 import * as fs from "fs";
 import * as path from "path";
 import { JSDocTsdParser } from "../src/core/jsdoc-tsd-parser";
 
 describe("JSDocTsdParser.parse.exampleProject", () => {
 	it("Should parse the example project", () => {
-		const a = execSync("npm run parse-example");
-		const data: TDoclet[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../exampleProject/jsdoc-results.json"), { encoding: "utf-8" }));
+		execSync("npm run parse-example");
+		const data: TDoclet[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../exampleProject/jsdoc-results.json"), { encoding: "utf-8" }));
 
 		const parser = new JSDocTsdParser();
 		parser.parse(data);
