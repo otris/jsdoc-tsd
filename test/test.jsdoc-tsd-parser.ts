8db02d5b14ddb12c0130468069d9f28ae1306d69
max <max@fax.de>
2019-08-16 23:49:43 +0200

    test for ignoring file annotations

--- test/test.jsdoc-tsd-parser.ts
+++ test/test.jsdoc-tsd-parser.ts
@@ -1,6 +1,7 @@
 import chai = require("chai");
 import { expect } from "chai";
 import { JSDocTsdParser } from "../src/core/jsdoc-tsd-parser";
+import { parseData } from "./jsdoc-helper";
 chai.should();
 
 describe("General tests for the parser", () => {
@@ -9,5 +10,17 @@ describe("General tests for the parser", () => {
 		expect(parser.getParsedItem.bind(parser, "notExisting"))
 			.to.throw(`Item with name 'notExisting' not found in result items`);
 	});
+
+	it("should not parse annotation 'file'", async () => {
+		const data = await parseData(`
+			/**
+			 * @file Fuuu
+			 */
+		`);
+
+		const parser = new JSDocTsdParser();
+		parser.parse(data);
+		const result = parser.resolveMembership();
+		expect(result.size).to.equal(0);
 	});
 });
